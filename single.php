b21931ff038f8080ace1541da4a6d001dc445489
Adam Butcher <adabutch1@gmail.com>
2018-09-27 12:37:54 -0400

    generically allow for old news posts without new acf

--- single.php
+++ single.php
@@ -12,36 +12,51 @@
   ?>
 
   <div class="container">
-    <img class="hero" src="<?php echo $bb_news_single_hero_image_url; ?>">
+    <div class="header-section">
+      <?php if( get_field('bb_news_single_hero_image_url') ): ?>
+        <img class="hero" src="<?php echo $bb_news_single_hero_image_url; ?>">
+      <?php endif; ?>
 
-    <h1><?php echo $bb_news_single_hero_heading; ?></h1>
-    <h4><?php echo $bb_news_single_hero_subheading; ?></h4>
-    <h4><?php echo $bb_news_single_hero_text; ?></h4>
+      <?php if( get_field('bb_news_single_hero_heading') ): ?>
+        <h1><?php echo $bb_news_single_hero_heading; ?></h1>
+      <?php elseif( ! empty( $post->post_title ) ): ?>
+        <h1><?php the_title(); ?></h1>
+      <?php else: endif; ?>
 
-    <?php if( have_rows('bb_news_text_sections') ): ?>
-      <?php while( have_rows('bb_news_text_sections') ): the_row(); ?>
-        <div class="item">
-          <h2><?php the_sub_field('bb_news_text_section_heading'); ?></h2>
-          <h3><?php the_sub_field('bb_news_text_section_subheading'); ?></h3>
-          <?php the_sub_field('bb_news_text_sections_text'); ?>
-        </div>
-      <?php endwhile; ?>
-    <?php endif; ?>
-  </div>
-
-  <div class="container">
-    <div class="carousel-wrap">
-      <div class="owl-carousel">
-        <?php if( have_rows('bb_news_gallery_images') ): ?>
-          <?php while( have_rows('bb_news_gallery_images') ): the_row(); ?>
-            <div class="item">
-              <img src="<?php the_sub_field('bb_news_gallery_image_url'); ?>">
-            </div>
-          <?php endwhile; ?>
-        <?php endif; ?>
+      <h4><?php if( get_field('bb_news_single_hero_subheading') ): echo $bb_news_single_hero_subheading; endif; ?></h4>
+      <h4><?php if( get_field('bb_news_single_hero_text') ): echo $bb_news_single_hero_text; endif; ?></h4>
       </div>
+
+      <?php if( have_rows('bb_news_text_sections') ): ?>
+        <?php while( have_rows('bb_news_text_sections') ): the_row(); ?>
+          <div class="item">
+            <h2><?php the_sub_field('bb_news_text_section_heading'); ?></h2>
+            <h3><?php the_sub_field('bb_news_text_section_subheading'); ?></h3>
+            <?php the_sub_field('bb_news_text_sections_text'); ?>
+          </div>
+        <?php endwhile; ?>
+      <?php endif; ?>
+
+      <?php the_content(); ?>
     </div>
   </div>
+
+  <?php if( get_field('bb_news_gallery_images') ): ?>
+    <div class="container">
+      <div class="carousel-wrap">
+        <div class="owl-carousel">
+          <?php if( have_rows('bb_news_gallery_images') ): ?>
+            <?php while( have_rows('bb_news_gallery_images') ): the_row(); ?>
+              <div class="item">
+                <img src="<?php the_sub_field('bb_news_gallery_image_url'); ?>">
+              </div>
+            <?php endwhile; ?>
+          <?php endif; ?>
+        </div>
+      </div>
+    </div>
+  <?php endif; ?>
+
   <?php endwhile; ?>
 </div>
 <?php include('subscribe-divider.php'); ?>
