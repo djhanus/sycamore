b7ef3b8825ec03d379f2c922d5e8c571b286d160
david-blueline <31481276+david-blueline@users.noreply.github.com>
2018-10-02 11:52:08 -0400

    changed footer logo from svg to png for clarity improvement

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </main>
   <footer>
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slt-logo-vertical.svg">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slt-logo-vertical.png">
     <?php wp_nav_menu( array( 'theme_location' => 'footer-social-menu' ) ); ?>
     <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
     <p>Sycamore Land Trust, <?php echo date("Y"); ?>. All rights reserved.</p>
