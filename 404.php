73d73453b441228e2500164b08f5ba520c29e59f
djhanus <davidjhanus@gmail.com>
2019-04-24 15:07:24 -0400

    add 404, fix staff image stretch

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header();?>
+<div class="donate single-page">
+  <div class="container">
+    <?php breadcrumbs(); ?>
+  </div>
+
+  <div class="container">
+    404 Error. Page not found.
+  </div>
+</div>
+<?php include('subscribe-divider.php'); ?>
+<?php get_footer(); ?>
