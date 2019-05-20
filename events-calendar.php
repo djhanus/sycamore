bf0884983c759d6f9018f8b38579b98438ace79b
cmsozio <cmsozio25@gmail.com>
2019-05-20 16:31:21 -0400

    changes to ACF for two possible link types

--- events-calendar.php
+++ events-calendar.php
@@ -36,6 +36,8 @@
       <?php if($the_query->have_posts() ) : while ( $the_query->have_posts() ) :
         $the_query->the_post();
         $bb_event_location               = get_field('bb_event_location', false, false);
+        $bb_event_external_location      = get_field('bb_event_external_location');
+        $bb_event_internal_location      = get_field('bb_event_internal_location');
         $bb_event_date                   = get_field('bb_event_date');
         $bb_event_facebook_event_url     = get_field('bb_event_facebook_event_url');
         $bb_event_rsvp                   = get_field('bb_event_rsvp');
@@ -55,9 +57,13 @@
 
             <?php if( get_field('bb_event_location') || get_field('bb_event_date') || get_field('bb_event_time_start') || get_field('bb_event_time_end') ): ?>
               <ul>
-                <?php if( get_field('bb_event_location') ): ?>
+                <?php if( get_field('bb_event_internal_location') ): ?>
                   <li><span class="location"></span>
-                    <a href="<?php echo get_the_permalink($bb_event_location); ?>"><?php echo get_the_title($bb_event_location); ?></a>
+                    <a href="<?php echo get_the_permalink($bb_event_internal_location); ?>"><?php echo get_the_title($bb_event_internal_location); ?></a>
+                  </li>
+                <?php elseif (get_field('bb_event_external_location') ): ?>
+                  <li><span class="location"></span>
+                    <a href="<?php echo get_the_permalink($bb_event_external_location); ?>"><?php echo get_the_title($bb_event_external_location); ?></a>
                   </li>
                 <?php endif; ?>
 
